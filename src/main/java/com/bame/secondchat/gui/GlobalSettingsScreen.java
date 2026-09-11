43dfb2980c8457ce8de62f19f3cafaf860f70ea7
Bameninghong9 <dubistfett2000@gmail.com>
2026-09-11 19:38:42 +0200

    Redesign Global Settings GUI to be 10000x better

--- src/main/java/com/bame/secondchat/gui/GlobalSettingsScreen.java
+++ src/main/java/com/bame/secondchat/gui/GlobalSettingsScreen.java
@@ -41,59 +41,68 @@ public class GlobalSettingsScreen extends Screen {
     protected void init() {
         super.init();
         
-        int startY = 75;
-        int fieldWidth = 200;
-        int fieldHeight = 20;
-        int xOffset = this.width / 2 - fieldWidth / 2;
+        com.bame.secondchat.config.GlobalConfig config = com.bame.secondchat.config.GlobalConfig.getInstance();
         
-        this.searchField = new TextFieldWidget(this.textRenderer, this.width / 2 - 100, 40, 200, 20, Text.literal("Search"));
-        this.searchField.setPlaceholder(Text.literal("Search settings..."));
+        int panelWidth = 280;
+        int panelX = (this.width - panelWidth) / 2;
+        int contentX = panelX + 20;
+        int contentWidth = 240;
+        int startY = 60;
+        
+        this.searchField = new TextFieldWidget(this.textRenderer, contentX, 35, contentWidth, 20, Text.literal("Search"));
+        this.searchField.setMaxLength(50);
+        this.searchField.setSuggestion("Search settings...");
+        this.searchField.setChangedListener(text -> {
+            if (text.isEmpty()) {
+                this.searchField.setSuggestion("Search settings...");
+            } else {
+                this.searchField.setSuggestion("");
+            }
+        });
         this.addDrawableChild(this.searchField);
         
-        GlobalConfig config = GlobalConfig.getInstance();
-        
-        this.maxMessagesField = new TextFieldWidget(this.textRenderer, xOffset, startY, fieldWidth, fieldHeight, Text.literal("Max Messages"));
+        this.maxMessagesField = new TextFieldWidget(this.textRenderer, contentX, startY, contentWidth, 20, Text.literal("Max Messages"));
         this.maxMessagesField.setText(String.valueOf(config.maxMessages));
         this.addDrawableChild(this.maxMessagesField);
         
-        this.timestampFormatField = new TextFieldWidget(this.textRenderer, xOffset, startY + 30, fieldWidth, fieldHeight, Text.literal("Timestamp Format"));
+        this.timestampFormatField = new TextFieldWidget(this.textRenderer, contentX, startY + 30, contentWidth, 20, Text.literal("Timestamp Format"));
         this.timestampFormatField.setMaxLength(50);
         this.timestampFormatField.setText(config.timestampFormat);
         this.addDrawableChild(this.timestampFormatField);
         
-        this.timestampColorField = new TextFieldWidget(this.textRenderer, xOffset, startY + 60, fieldWidth, fieldHeight, Text.literal("Timestamp Color"));
-        this.timestampColorField.setMaxLength(20);
+        this.timestampColorField = new TextFieldWidget(this.textRenderer, contentX + 25, startY + 60, contentWidth - 25 - 55, 20, Text.literal("Timestamp Color"));
+        this.timestampColorField.setMaxLength(9);
         this.timestampColorField.setText(config.timestampColor);
         this.addDrawableChild(this.timestampColorField);
         
-        this.selectionColorField = new TextFieldWidget(this.textRenderer, xOffset, startY + 90, fieldWidth, fieldHeight, Text.literal("Selection Color"));
-        this.selectionColorField.setMaxLength(20);
+        this.timestampColorResetButton = ButtonWidget.builder(Text.literal("Reset"), button -> {
+            this.timestampColorField.setText("#AAAAAA");
+        }).dimensions(contentX + contentWidth - 50, startY + 60, 50, 20).build();
+        this.addDrawableChild(this.timestampColorResetButton);
+        
+        this.selectionColorField = new TextFieldWidget(this.textRenderer, contentX + 25, startY + 90, contentWidth - 25 - 55, 20, Text.literal("Selection Color"));
+        this.selectionColorField.setMaxLength(9);
         this.selectionColorField.setText(config.selectionColor);
         this.addDrawableChild(this.selectionColorField);
         
-        this.stackMessagesField = new TextFieldWidget(this.textRenderer, xOffset, startY + 120, fieldWidth, fieldHeight, Text.literal("Stack Messages"));
+        this.selectionColorResetButton = ButtonWidget.builder(Text.literal("Reset"), button -> {
+            this.selectionColorField.setText("#5555FF");
+        }).dimensions(contentX + contentWidth - 50, startY + 90, 50, 20).build();
+        this.addDrawableChild(this.selectionColorResetButton);
+        
+        this.stackMessagesField = new TextFieldWidget(this.textRenderer, contentX, startY + 120, contentWidth, 20, Text.literal("Stack Messages"));
         this.stackMessagesField.setText(String.valueOf(config.stackMessages));
         this.addDrawableChild(this.stackMessagesField);
         
-        this.timestampColorResetButton = ButtonWidget.builder(Text.literal("Reset"), button -> {
-            this.timestampColorField.setText("§7");
-        }).dimensions(xOffset + fieldWidth + 5, startY + 60, 50, 20).build();
-        this.addDrawableChild(this.timestampColorResetButton);
-        
-        this.selectionColorResetButton = ButtonWidget.builder(Text.literal("Reset"), button -> {
-            this.selectionColorField.setText("#880000FF");
-        }).dimensions(xOffset + fieldWidth + 5, startY + 90, 50, 20).build();
-        this.addDrawableChild(this.selectionColorResetButton);
-        
-        Text initialText = Text.literal(com.bame.secondchat.config.ModConfig.showFontDropdown ? "Yes" : "No")
+        Text initialFontText = Text.literal(com.bame.secondchat.config.ModConfig.showFontDropdown ? "Yes" : "No")
             .withColor(com.bame.secondchat.config.ModConfig.showFontDropdown ? 0x00FF00 : 0xFF0000);
             
-        this.showFontDropdownButton = ButtonWidget.builder(initialText, button -> {
+        this.showFontDropdownButton = ButtonWidget.builder(initialFontText, button -> {
             com.bame.secondchat.config.ModConfig.showFontDropdown = !com.bame.secondchat.config.ModConfig.showFontDropdown;
             Text newText = Text.literal(com.bame.secondchat.config.ModConfig.showFontDropdown ? "Yes" : "No")
                 .withColor(com.bame.secondchat.config.ModConfig.showFontDropdown ? 0x00FF00 : 0xFF0000);
             button.setMessage(newText);
-        }).dimensions(xOffset, startY + 150, fieldWidth, 20).build();
+        }).dimensions(contentX, startY + 150, contentWidth, 20).build();
         this.addDrawableChild(this.showFontDropdownButton);
         
         Text initialEmojiText = Text.literal(com.bame.secondchat.config.ModConfig.showEmojiButton ? "Yes" : "No")
@@ -104,7 +113,7 @@ public class GlobalSettingsScreen extends Screen {
             Text newText = Text.literal(com.bame.secondchat.config.ModConfig.showEmojiButton ? "Yes" : "No")
                 .withColor(com.bame.secondchat.config.ModConfig.showEmojiButton ? 0x00FF00 : 0xFF0000);
             button.setMessage(newText);
-        }).dimensions(xOffset, startY + 180, fieldWidth, 20).build();
+        }).dimensions(contentX, startY + 180, contentWidth, 20).build();
         this.addDrawableChild(this.showEmojiButtonButton);
         
         Text initialHeadsText = Text.literal(com.bame.secondchat.config.ModConfig.showPlayerHeads ? "Yes" : "No")
@@ -115,10 +124,10 @@ public class GlobalSettingsScreen extends Screen {
             Text newText = Text.literal(com.bame.secondchat.config.ModConfig.showPlayerHeads ? "Yes" : "No")
                 .withColor(com.bame.secondchat.config.ModConfig.showPlayerHeads ? 0x00FF00 : 0xFF0000);
             button.setMessage(newText);
-        }).dimensions(xOffset, startY + 210, fieldWidth, 20).build();
+        }).dimensions(contentX, startY + 210, contentWidth, 20).build();
         this.addDrawableChild(this.showPlayerHeadsButton);
 
-        this.opacitySlider = new net.minecraft.client.gui.widget.SliderWidget(xOffset, startY + 240, fieldWidth, 20, Text.literal("Text Background Opacity: " + config.chatBackgroundOpacity + "%"), config.chatBackgroundOpacity / 100.0) {
+        this.opacitySlider = new net.minecraft.client.gui.widget.SliderWidget(contentX, startY + 240, contentWidth, 20, Text.literal("Text Background Opacity: " + config.chatBackgroundOpacity + "%"), config.chatBackgroundOpacity / 100.0) {
             @Override
             protected void updateMessage() {
                 this.setMessage(Text.literal("Text Background Opacity: " + (int)(this.value * 100) + "%"));
@@ -131,10 +140,12 @@ public class GlobalSettingsScreen extends Screen {
         };
         this.addDrawableChild(this.opacitySlider);
         
+        int panelHeight = Math.max(300, this.height - 40);
+        int panelY = 20;
         this.saveButton = ButtonWidget.builder(Text.literal("Save & Close"), button -> {
             saveSettings();
             this.client.setScreen(this.parent);
-        }).dimensions(this.width / 2 - 100, this.height - 30, 200, 20).build();
+        }).dimensions(contentX, panelY + panelHeight - 30, contentWidth, 20).build();
         
         this.addDrawableChild(this.saveButton);
         
@@ -160,14 +171,15 @@ public class GlobalSettingsScreen extends Screen {
             }
         }
         
-        int fieldWidth = 200;
-        int xOffset = this.width / 2 - fieldWidth / 2;
+        int panelWidth = 280;
+        int panelX = (this.width - panelWidth) / 2;
+        int contentX = panelX + 20;
         
         if (this.timestampColorField.visible) {
             int y = this.timestampColorField.getY();
-            if (mouseX >= xOffset - 26 && mouseX <= xOffset - 4 && mouseY >= y - 1 && mouseY <= y + 21) {
+            if (mouseX >= contentX && mouseX <= contentX + 20 && mouseY >= y - 1 && mouseY <= y + 21) {
                 activePickerField = "timestamp";
-                colorPicker.setPosition(xOffset - 150, y);
+                colorPicker.setPosition(contentX - 140, y);
                 colorPicker.setColor(this.timestampColorField.getText());
                 colorPicker.setVisible(true);
                 return true;
@@ -176,9 +188,9 @@ public class GlobalSettingsScreen extends Screen {
         
         if (this.selectionColorField.visible) {
             int y = this.selectionColorField.getY();
-            if (mouseX >= xOffset - 26 && mouseX <= xOffset - 4 && mouseY >= y - 1 && mouseY <= y + 21) {
+            if (mouseX >= contentX && mouseX <= contentX + 20 && mouseY >= y - 1 && mouseY <= y + 21) {
                 activePickerField = "selection";
-                colorPicker.setPosition(xOffset - 150, y);
+                colorPicker.setPosition(contentX - 140, y);
                 colorPicker.setColor(this.selectionColorField.getText());
                 colorPicker.setVisible(true);
                 return true;
@@ -291,56 +303,57 @@ public class GlobalSettingsScreen extends Screen {
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
         super.render(context, mouseX, mouseY, delta);
         
-        // Draw title
-        context.drawCenteredTextWithShadow(this.textRenderer, this.title, this.width / 2, 25, 0xFFFFAA00);
+        int panelWidth = 280;
+        int panelHeight = Math.max(300, this.height - 40);
+        int panelX = (this.width - panelWidth) / 2;
+        int panelY = 20;
         
-        int startY = 75;
-        int fieldWidth = 200;
-        int xOffset = this.width / 2 - fieldWidth / 2;
+        context.fill(panelX, panelY, panelX + panelWidth, panelY + panelHeight, 0xCC000000);
+        context.fill(panelX - 1, panelY - 1, panelX + panelWidth + 1, panelY, 0xFF555555);
+        context.fill(panelX - 1, panelY + panelHeight, panelX + panelWidth + 1, panelY + panelHeight + 1, 0xFF555555);
+        context.fill(panelX - 1, panelY, panelX, panelY + panelHeight, 0xFF555555);
+        context.fill(panelX + panelWidth, panelY, panelX + panelWidth + 1, panelY + panelHeight, 0xFF555555);
+        
+        context.drawCenteredTextWithShadow(this.textRenderer, Text.literal("Global Settings").withColor(0xFFFFAA00).append(Text.literal(" - SecondChat").withColor(0xFFFFFFFF)), this.width / 2, panelY + 10, 0xFFFFFFFF);
         
         String search = this.searchField.getText().toLowerCase();
         
-        // Filter logic for drawing
         boolean showMaxMsg = "max messages".contains(search) || search.isEmpty();
+        boolean showStackMsg = "stack messages".contains(search) || search.isEmpty();
         boolean showTsFormat = "timestamp format".contains(search) || search.isEmpty();
         boolean showTsColor = "timestamp color".contains(search) || search.isEmpty();
         boolean showSelColor = "selection color".contains(search) || search.isEmpty();
-        boolean showStackMsg = "stack messages".contains(search) || search.isEmpty();
+        boolean showFontDropdownSetting = "show font dropdown".contains(search) || search.isEmpty();
+        boolean showEmojiBtnSetting = "show emoji button".contains(search) || search.isEmpty();
+        boolean showHeadsBtnSetting = "show player heads".contains(search) || search.isEmpty();
+        boolean showOpacitySetting = "text background opacity".contains(search) || search.isEmpty();
         
         this.maxMessagesField.visible = showMaxMsg;
+        this.stackMessagesField.visible = showStackMsg;
         this.timestampFormatField.visible = showTsFormat;
         this.timestampColorField.visible = showTsColor;
-        this.selectionColorField.visible = showSelColor;
-        this.stackMessagesField.visible = showStackMsg;
-        
         this.timestampColorResetButton.visible = showTsColor;
+        this.selectionColorField.visible = showSelColor;
         this.selectionColorResetButton.visible = showSelColor;
-        
-        boolean showFontDropdownSetting = "font dropdown".contains(search) || search.isEmpty();
         this.showFontDropdownButton.visible = showFontDropdownSetting;
-        
-        boolean showEmojiBtnSetting = "emoji button".contains(search) || search.isEmpty();
         this.showEmojiButtonButton.visible = showEmojiBtnSetting;
-        
-        boolean showHeadsBtnSetting = "player heads skin".contains(search) || search.isEmpty();
         this.showPlayerHeadsButton.visible = showHeadsBtnSetting;
-        
-        boolean showOpacitySetting = "opacity deckkraft background".contains(search) || search.isEmpty();
         this.opacitySlider.visible = showOpacitySetting;
         
-        int currentY = startY;
+        int contentX = panelX + 20;
+        int currentY = 60;
         
         if (search.isEmpty()) {
             context.drawCenteredTextWithShadow(this.textRenderer, Text.literal("--- Chat Behavior ---").withColor(0xFFFFFF55), this.width / 2, currentY, 0xFFFFFF55);
             currentY += 20;
         }
         if (showMaxMsg) {
-            context.drawTextWithShadow(this.textRenderer, Text.literal("Max Messages (0 = unendlich)"), xOffset, currentY - 10, 0xFFAAAAAA);
+            context.drawTextWithShadow(this.textRenderer, Text.literal("Max Messages (0 = unendlich)"), contentX, currentY - 10, 0xFFAAAAAA);
             this.maxMessagesField.setY(currentY);
             currentY += 35;
         }
         if (showStackMsg) {
-            context.drawTextWithShadow(this.textRenderer, Text.literal("Stack Messages (0 = aus)"), xOffset, currentY - 10, 0xFFAAAAAA);
+            context.drawTextWithShadow(this.textRenderer, Text.literal("Stack Messages (0 = aus)"), contentX, currentY - 10, 0xFFAAAAAA);
             this.stackMessagesField.setY(currentY);
             currentY += 35;
         }
@@ -351,32 +364,32 @@ public class GlobalSettingsScreen extends Screen {
             currentY += 20;
         }
         if (showTsFormat) {
-            context.drawTextWithShadow(this.textRenderer, Text.literal("Timestamp Format"), xOffset, currentY - 10, 0xFFAAAAAA);
+            context.drawTextWithShadow(this.textRenderer, Text.literal("Timestamp Format"), contentX, currentY - 10, 0xFFAAAAAA);
             this.timestampFormatField.setY(currentY);
             currentY += 35;
         }
         if (showTsColor) {
-            context.drawTextWithShadow(this.textRenderer, Text.literal("Timestamp Color"), xOffset, currentY - 10, 0xFFAAAAAA);
+            context.drawTextWithShadow(this.textRenderer, Text.literal("Timestamp Color"), contentX, currentY - 10, 0xFFAAAAAA);
             this.timestampColorField.setY(currentY);
             this.timestampColorResetButton.setY(currentY);
             
             int color = parseColorForPreview(this.timestampColorField.getText());
-            context.fill(xOffset - 26, currentY - 1, xOffset - 4, currentY + 21, 0xFFFFFFFF);
-            context.fill(xOffset - 25, currentY, xOffset - 5, currentY + 20, color);
+            context.fill(contentX - 1, currentY - 1, contentX + 21, currentY + 21, 0xFFFFFFFF);
+            context.fill(contentX, currentY, contentX + 20, currentY + 20, color);
             currentY += 35;
         }
         if (showSelColor) {
-            context.drawTextWithShadow(this.textRenderer, Text.literal("Selection Color (Hex)"), xOffset, currentY - 10, 0xFFAAAAAA);
+            context.drawTextWithShadow(this.textRenderer, Text.literal("Selection Color (Hex)"), contentX, currentY - 10, 0xFFAAAAAA);
             this.selectionColorField.setY(currentY);
             this.selectionColorResetButton.setY(currentY);
             
             int color = parseColorForPreview(this.selectionColorField.getText());
-            context.fill(xOffset - 26, currentY - 1, xOffset - 4, currentY + 21, 0xFFFFFFFF);
-            context.fill(xOffset - 25, currentY, xOffset - 5, currentY + 20, color);
+            context.fill(contentX - 1, currentY - 1, contentX + 21, currentY + 21, 0xFFFFFFFF);
+            context.fill(contentX, currentY, contentX + 20, currentY + 20, color);
             currentY += 35;
         }
         if (showOpacitySetting) {
-            context.drawTextWithShadow(this.textRenderer, Text.literal("Text Background Opacity"), xOffset, currentY - 10, 0xFFAAAAAA);
+            context.drawTextWithShadow(this.textRenderer, Text.literal("Text Background Opacity"), contentX, currentY - 10, 0xFFAAAAAA);
             this.opacitySlider.setY(currentY);
             currentY += 35;
         }
@@ -387,17 +400,17 @@ public class GlobalSettingsScreen extends Screen {
             currentY += 20;
         }
         if (showFontDropdownSetting) {
-            context.drawTextWithShadow(this.textRenderer, Text.literal("Show Font Dropdown"), xOffset, currentY - 10, 0xFFAAAAAA);
+            context.drawTextWithShadow(this.textRenderer, Text.literal("Show Font Dropdown"), contentX, currentY - 10, 0xFFAAAAAA);
             this.showFontDropdownButton.setY(currentY);
             currentY += 35;
         }
         if (showEmojiBtnSetting) {
-            context.drawTextWithShadow(this.textRenderer, Text.literal("Show Emoji Button"), xOffset, currentY - 10, 0xFFAAAAAA);
+            context.drawTextWithShadow(this.textRenderer, Text.literal("Show Emoji Button"), contentX, currentY - 10, 0xFFAAAAAA);
             this.showEmojiButtonButton.setY(currentY);
             currentY += 35;
         }
         if (showHeadsBtnSetting) {
-            context.drawTextWithShadow(this.textRenderer, Text.literal("Show Player Heads"), xOffset, currentY - 10, 0xFFAAAAAA);
+            context.drawTextWithShadow(this.textRenderer, Text.literal("Show Player Heads"), contentX, currentY - 10, 0xFFAAAAAA);
             this.showPlayerHeadsButton.setY(currentY);
             currentY += 35;
         }
